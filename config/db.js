ef36fd43f1d13552749bc90553809b408054e13d
danielpinoy <almirante.danieljohn@gmail.com>
2025-03-04 00:38:59 +0200

    added heruko in db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,35 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "task_manager",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// local
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME || "task_manager",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// heruko
+let pool;
+
+// Check if running on Heroku (with JAWSDB)
+if (process.env.JAWSDB_URL) {
+  pool = mysql.createPool(process.env.JAWSDB_URL);
+} else {
+  // Local development connection
+  pool = mysql.createPool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || "task_manager",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+}
 
 module.exports = pool;
